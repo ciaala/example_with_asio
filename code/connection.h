463d4faf9c730f35edac975bba778050cca7b876
crypt <francesco.fiduccia@gmail.com>
2017-11-07 20:49:19 +0100

    make the server replay the messages on all the connections.

--- code/connection.h
+++ code/connection.h
@@ -10,57 +10,76 @@
 #include <iostream>
 
 struct connection
-  : std::enable_shared_from_this<connection> {
-  connection(asio::ip::tcp::socket sock)
-    : m_sock{std::move(sock)}
-    , m_strand{m_sock.get_io_service()} {
+        : std::enable_shared_from_this<connection> {
 
-  }
 
-  void start() {
-    do_read();
-  }
+    // Constructor
+    connection(asio::ip::tcp::socket sock, std::vector<std::shared_ptr<connection>> &connections) :
+            m_sock{std::move(sock)},
+            m_strand{m_sock.get_io_service()},
+            connections(connections) {
 
-  private:
+    }
+
+    void start() {
+        do_read();
+    }
+    asio::ip::tcp::socket& getSocket() {
+        return m_sock;
+    }
+
+private:
+
     void write(std::string data) {
-      m_out.push_back(data);
+        m_out.push_back(data);
 
-      if(m_out.size() < 2) {
-        do_write();
-      }
+        if (m_out.size() < 2) {
+            do_write();
+        }
     }
 
     void do_read() {
-      m_sock.async_read_some(asio::buffer(m_buff),
-        [&, self = shared_from_this()](auto error, auto read) {
-        if(!error) {
-          auto begin = m_buff.data();
-          auto end = begin + read;
-          m_strand.post([&, data=std::string{begin, end}]{
-            write(data);
-          });
-            do_read();
-        }
-      });
+        m_sock.async_read_some(asio::buffer(m_buff),
+                               [&, self = shared_from_this()](auto error, auto read) {
+                                   if (!error) {
+                                       auto begin = m_buff.data();
+                                       auto end = begin + read;
+                                       m_strand.post([&, data = std::string{begin, end}] {
+                                           write(data);
+                                       });
+                                       do_read();
+                                   }
+                               });
     }
 
     void do_write() {
-      asio::async_write(m_sock, asio::buffer(m_out.front()),
-        m_strand.wrap([&, self = shared_from_this()](auto error, auto written) {
+        bool connection_error = false;
+        for (auto conn : connections) {
+            //TODO Write the same message to multiple sockets
+            asio::async_write(conn->getSocket(),
+                              asio::buffer(m_out.front()),
+                              m_strand.wrap(
+                                      [&, self = shared_from_this()](auto error, auto written) {
+                                        connection_error = connection_error || error;
+                                      }));
+        }
         m_out.pop_front();
 
-        if(!error) {
-          if(!m_out.empty()) {
-            do_write();
-          }
+        if (!connection_error) {
+            if (!m_out.empty()) {
+                do_write();
+            }
+        } else {
+            std::cout << "Error " << std::endl;
         }
-      }));
     }
 
     asio::ip::tcp::socket m_sock;
     asio::strand m_strand;
     std::array<char, 1024> m_buff{};
     std::deque<std::string> m_out{};
-  };
+    std::vector<std::shared_ptr<connection>> &connections;
+
+};
 
 #endif
--- code/echo.cpp
+++ code/echo.cpp
@@ -5,59 +5,74 @@
 #include <signal.h>
 #include <thread>
 #include <vector>
+#include <string>
 
-struct server
-  {
-  server(asio::io_service & service)
-    : m_acceptor{service,
-                 asio::ip::tcp::endpoint{
-                   asio::ip::tcp::v4(),
-                   4321}}
-    , m_sock{service} {
+struct server {
+    // Constructor
+    server(asio::io_service &service)
+            : m_acceptor{service,
+                         asio::ip::tcp::endpoint{
+                                 asio::ip::tcp::v4(),
+                                 4321}}, server_socket{service} {
     }
 
     void start() {
-      do_accept();
+        do_accept();
     }
 
-  private:
+private:
     void do_accept() {
-      m_acceptor.async_accept(m_sock, [&](auto error){
-        if(!error) {
-          auto conn = std::make_shared<connection>(std::move(m_sock));
-          conn->start();
+        m_acceptor.async_accept(server_socket, [&](auto error) {
+            if (!error) {
+                auto conn = std::make_shared<connection>(std::move(server_socket), connections);
+                std::cout << "New connection accepted." << std::endl;
+                connections.push_back(conn);
 
-          do_accept();
-        }
-      });
+                conn->start();
+
+                do_accept();
+            }
+        });
     }
 
+    std::vector<std::shared_ptr<connection>> connections;
     asio::ip::tcp::acceptor m_acceptor;
-    asio::ip::tcp::socket m_sock;
-  };
+    asio::ip::tcp::socket server_socket;
+};
 
-int main() {
-  auto && service = asio::io_service{};
-  auto && server = ::server{service};
+void test_message() {
+    std::string str = "test";
+    std::vector<std::string> vectors;
+    vectors.push_back(std::move(str));
 
-  server.start();
+    std::cout << "No nullptr '" << str << "'" << std::endl;
 
-  auto && signals = asio::signal_set(service, SIGINT);
-  signals.add(SIGINT);
-  signals.async_wait([&](auto error, auto signal){
-    if(!error && !service.stopped()) {
-      std::cout << "Received SIGINT. Terminating. " << '\n';
-      service.stop();
-    }
-  });
+}
 
-  auto const cpus = std::thread::hardware_concurrency();
-  auto pool = std::vector<std::future<void>>{cpus};
+int main(int argc, char **argv) {
 
-  for(auto & future : pool)
-    {
-    future = std::async(std::launch::async, [&]{
-      service.run();
+    // test_message();
+
+    auto &&service = asio::io_service{};
+    auto &&server = ::server{service};
+
+    server.start();
+
+    auto &&signals = asio::signal_set(service, SIGINT);
+    signals.add(SIGINT);
+    signals.async_wait([&](auto error, auto signal) {
+        if (!error && !service.stopped()) {
+            std::cout << "Received SIGINT. Terminating. " << '\n';
+            service.stop();
+        }
     });
+
+    auto const cpus = std::thread::hardware_concurrency();
+    auto pool = std::vector<std::future<void>>{cpus};
+
+    for (auto &future : pool) {
+        future = std::async(std::launch::async, [&] {
+            service.run();
+        });
     }
-  }
+}
